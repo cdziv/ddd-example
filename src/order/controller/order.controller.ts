2513692e45aae6fe9617d8b99f8ef6483b102805
cdziv <cdziv>
2024-07-25 17:02:59 +0800

    test: order e2e-tests

--- src/order/controller/order.controller.ts
+++ src/order/controller/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { OrderDtoAssembler, OrderService } from '../application';
 import { OrderCreateBody, OrderResponse } from '../api-interfaces';
 
@@ -10,6 +10,7 @@ export class OrderController {
   ) {}
 
   @Post()
+  @HttpCode(201)
   async createOrder(@Body() body: OrderCreateBody): Promise<OrderResponse> {
     // 透過 Dto Assembler 將 API 的資料轉換成應用層的物件，驗證資料型別的過程放在 DtoAssembler 裡面。
     const params = this.orderDtoAssembler.orderCreateBodyToParams(body);
