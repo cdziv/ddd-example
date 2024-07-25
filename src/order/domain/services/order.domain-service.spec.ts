1dab75d5f355dea078cbd5af13072a48c156a38d
cdziv <cdziv>
2024-07-26 00:20:40 +0800

    test: unit test for OrderDomainService.exchange

--- src/order/domain/services/order.domain-service.spec.ts
+++ src/order/domain/services/order.domain-service.spec.ts
@@ -1,85 +1,29 @@
 import { TestBed } from '@automock/jest';
 import { OrderDomainService } from './order.domain-service';
+import { Currency, DecimalAmount, ExchangeRate, PriceV2 } from '../vo';
+import { CurrencyType } from '../../order.constants';
 
 describe('OrderDomainService', () => {
   const { unit: orderDomainService } =
     TestBed.create(OrderDomainService).compile();
 
-  it('should be defined', () => {
-    expect(orderDomainService).toBeDefined();
+  describe('exchange', () => {
+    it('When passing originalAmount value is 5, and exchangeRate is 10.5 and currency is TWD, it should return Price with amount 52.5 and currency TWD', () => {
+      const originalAmount = DecimalAmount.create(5);
+      const exchangeRate = new ExchangeRate({
+        from: new Currency({ value: CurrencyType.USD }),
+        to: new Currency({ value: CurrencyType.TWD }),
+        rate: DecimalAmount.create(10.5),
+      });
+
+      const result = orderDomainService.exchange(originalAmount, exchangeRate);
+
+      expect(result).toEqual(
+        new PriceV2({
+          amount: DecimalAmount.create(52.5),
+          currency: new Currency({ value: CurrencyType.TWD }),
+        }),
+      );
+    });
   });
-
-  // describe('switchToTWDCurrencyOrder', () => {
-  //   it('should switch USD to TWD', async () => {
-  //     const currencyUSD = new Currency({ value: CurrencyType.USD });
-  //     const originalPrice = Price.create(faker.finance.amount());
-  //     const exchangeRateProvider = unitRef.get<MockExchangeRateProvider>(
-  //       EXCHANGE_RATE_PROVIDER,
-  //     );
-  //     const rate = await exchangeRateProvider.usdToTwd();
-  //     const order = OrderAR.create({
-  //       name: new OrderName({ value: generateCapitalizedWords(3, 5) }),
-  //       address: new Address({
-  //         city: new City({ value: faker.word.words(2) }),
-  //         district: new District({ value: faker.word.words(2) }),
-  //         street: new Street({ value: faker.word.words(3) }),
-  //       }),
-  //       price: originalPrice,
-  //       currency: currencyUSD,
-  //     });
-
-  //     const updatedOrder =
-  //       await orderDomainService.switchToTWDCurrencyOrder(order);
-  //     const expectedCurrency = new Currency({ value: CurrencyType.TWD });
-  //     const expectedPrice = originalPrice.mul(rate);
-
-  //     expect(updatedOrder.props.currency).toEqual(expectedCurrency);
-  //     expect(updatedOrder.props.price).toEqual(expectedPrice);
-  //   });
-  //   it('When currency is TWD, should return original order', async () => {
-  //     const currencyTWD = new Currency({ value: CurrencyType.TWD });
-  //     const originalPrice = Price.create(faker.finance.amount());
-  //     const order = OrderAR.create({
-  //       name: new OrderName({ value: generateCapitalizedWords(3, 5) }),
-  //       address: new Address({
-  //         city: new City({ value: faker.word.words(2) }),
-  //         district: new District({ value: faker.word.words(2) }),
-  //         street: new Street({ value: faker.word.words(3) }),
-  //       }),
-  //       price: originalPrice,
-  //       currency: currencyTWD,
-  //     });
-
-  //     const updatedOrder =
-  //       await orderDomainService.switchToTWDCurrencyOrder(order);
-
-  //     expect(updatedOrder).toBe(order);
-  //   });
-  // });
-
-  // describe('switchUSDToTWD', () => {
-  //   it('should switch USD to TWD', async () => {
-  //     const currencyUSD = new Currency({ value: CurrencyType.USD });
-  //     const originalPrice = Price.create(faker.finance.amount());
-  //     const exchangeRateProvider = unitRef.get<MockExchangeRateProvider>(
-  //       EXCHANGE_RATE_PROVIDER,
-  //     );
-  //     const rate = await exchangeRateProvider.usdToTwd();
-
-  //     const newPrice = await orderDomainService.switchUSDToTWD(
-  //       currencyUSD,
-  //       originalPrice,
-  //     );
-
-  //     expect(newPrice).toEqual(originalPrice.mul(rate));
-  //   });
-  //   it('When currency is not USD, should throw ParamInvalidDomainError', async () => {
-  //     const currencyTWD = new Currency({ value: CurrencyType.TWD });
-  //     const originalPrice = Price.create(faker.finance.amount());
-
-  //     await expect(
-  //       orderDomainService.switchUSDToTWD(currencyTWD, originalPrice),
-  //     ).rejects.toThrow('Invalid currency');
-  //   });
-  // });
 });
